f26a0588bfc6c4c14ea18dfbc2ef4e61992c483a
Jaewon Choi <qwebnm7788@naver.com>
2017-11-12 20:24:38 +0900

    final fix

--- aes.cpp
+++ aes.cpp
@@ -1,3 +1,15 @@
+/*
+
+컴퓨터 보안
+
+Modified AES-128
+
+12131619 컴퓨터공학과 최재원
+
+2017. 11. 12
+
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,48 +17,7 @@
 #include <algorithm>
 using namespace std;
 
- //debugging SBox
-static const uint8_t r_sbox[256] = {
-	//0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
-	0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
-	0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
-	0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
-	0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
-	0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
-	0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
-	0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
-	0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
-	0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
-	0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
-	0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
-	0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
-	0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
-	0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
-	0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
-	0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 };
-	
-//debugging InvSbox
-static const uint8_t aes_sbox_inv_table[256u] =
-{
-	0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
-	0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
-	0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
-	0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
-	0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
-	0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
-	0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
-	0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
-	0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
-	0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
-	0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
-	0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
-	0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
-	0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
-	0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
-	0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D
-};
-
-
+//Sbox
 static const unsigned char SBox[256] = {
 	0xD4, 0xAD, 0x82, 0x7D, 0xA2, 0x59, 0xF0, 0xAF, 0x9C, 0xA4, 0x72, 0xC0, 0xCA, 0xC9, 0xFA, 0x47,
 	0xA5, 0x34, 0xFD, 0x26, 0xE5, 0x3F, 0xCC, 0xF1, 0x71, 0xD8, 0x31, 0x15, 0xB7, 0x93, 0x36, 0xF7,
@@ -66,22 +37,25 @@ static const unsigned char SBox[256] = {
 	0x3B, 0x52, 0x83, 0x1A, 0xD6, 0x6E, 0xA0, 0xB3, 0x29, 0xE3, 0x2F, 0x84, 0x09, 0x2C, 0x1B, 0x5A
 };
 
+//파일 이름 정보
 const string plainTextFile= "pt.bin";
 const string keyFile = "key.bin";
 const string cipherTextFile = "ct.bin";
 const string decrpytFile = "pt2.bin";
 
-const int BUFSIZE = 512;
-const int BLKSIZE = 16;
-const int EXPANDEDKEY = 44;
-const int WORDSIZE = 4;
+//사용할 상수 정보
+const int BLKSIZE = 16;					//block size
+const int EXPANDEDKEY = 44;				//number of expansion keys
+const int WORDSIZE = 4;					//size of WORD
 const int MOD = 27;			//= 11011(2) (x^4 + x^3 + x + 1)
-const int NUMOFROUNDS = 11;
+const int NUMOFROUNDS = 11;				//number of rounds
 
+//WORD 데이터 타입의 정의
 class WORD {
 public:
 	unsigned char bytes[WORDSIZE];			//1word = 4 bytes   (abcd)
-	//4비트 입력을 하나의 word로 바꾸어 준다.
+
+	//생성자를 이용하여 4비트 입력을 하나의 word로 바꾸어 준다.
 	WORD(unsigned char a = 0, unsigned char b = 0, unsigned char c = 0, unsigned char d = 0) {
 		bytes[0] = a;
 		bytes[1] = b;
@@ -89,10 +63,11 @@ public:
 		bytes[3] = d;
 	}
 
+	//두 WORD 타입 사이의 덧셈 연산을 쉽게 만들어 준다.
 	WORD operator+(const WORD& ref) {
 		WORD ret;
 		for (int i = 0; i < WORDSIZE; ++i) {
-			ret.bytes[i] = (this->bytes[i] ^ ref.bytes[i]);
+			ret.bytes[i] = (this->bytes[i] ^ ref.bytes[i]);				//각 WORD 사이의 모든 byte들을 XOR 연산해 준다.
 		}
 		return ret;
 	}
@@ -105,12 +80,10 @@ unsigned char multInGf(unsigned char a, unsigned char b) {
 	for (int i = 0; i < 8; ++i) {
 		if (b & (1 << i)) {
 			//i번 비트가 set되어 있는 경우 덧셈을 진행한다.
-			if (result == 0) {
+			if (result == 0)
 				result = a;
-			}
-			else {
+			else
 				result ^= a;
-			}
 		}
 
 		if (a & (1 << 7)) {
@@ -128,7 +101,7 @@ unsigned char multInGf(unsigned char a, unsigned char b) {
 	return result;
 }
 
-
+//addRoundKey -> 주어진 roundKey를 state에 더해준다.
 void addRoundKey(const WORD* roundKey, unsigned char state[]) {
 	for (int i = 0; i < WORDSIZE; ++i) {
 		state[4 * i] ^= (roundKey + i)->bytes[0];
@@ -138,24 +111,25 @@ void addRoundKey(const WORD* roundKey, unsigned char state[]) {
 	}
 }
 
+//invAddRoundKey -> addRoundKey의 역연산
 void invAddRoundKey(const WORD* roundkey, unsigned char state[]) {
-	//xor연산의 특성을 이용하여 역연산을 진행
+	//xor연산의 특성을 이용하여 순방향의 연산을 한번 더 진행함으로써 역연산의 효과를 얻음
 	addRoundKey(roundkey, state);
 }
 
+//subByte -> state의 모든 byte를 Sbox를 통해 mapping 해준다.
 void subByte(unsigned char state[]) {
 	for (int i = 0; i < BLKSIZE; ++i) {
+		//(16 * 상위 4비트의 표현 값) 을 행으로 (하위 4비트의 표현 값)을 열로 사용하는 점을 통해
+		//다음과 같은 공식으로 Sbox에 접근해줄 수 있다.
 		state[i] = SBox[(state[i] & 0xF0) + (state[i] & 0x0F)];
-		//state[i] = r_sbox[(state[i] & 0xf0) + (state[i] & 0x0f)];
 	}
 }
 
+//invSubByte -> subByte의 역연산
 void invSubByte(unsigned char state[]) {
-	/* //debug
-	for (int i = 0; i < BLKSIZE; ++i) {
-		state[i] = aes_sbox_inv_table[(state[i] & 0xf0) + (state[i] & 0x0f)];
-	}
-	*/
+	//reverse Sbox은 SBox의 반대되는 mapping을 정의하기 때문에 매번 256번의 루프를 돌며
+	//Sbox내 현재 바이트값과 동일한 값을 찾은 후 그 때의 index가 그 값이 된다.
 	int found;
 	for (int i = 0; i < BLKSIZE; ++i) {
 		for (int j = 0; j < 256; ++j) {
@@ -163,26 +137,15 @@ void invSubByte(unsigned char state[]) {
 				found = j;
 			}
 		}
-		state[i] = ((found / BLKSIZE) << 4) + (found % BLKSIZE);
+		state[i] = ((found / BLKSIZE) << 4) + (found % BLKSIZE);			//찾은 index를 값으로 환산
 	}
 }
 
+//shiftRow -> 명시된 대로 주어진 상태의 각 행에 대해 적절한 수만큼의 오른쪽 시프트 연산을 진행함.
 void shiftRow(unsigned char state[]) {
-	/* //debug
 	int hold;
 	for (int i = 0; i < 4; ++i) {
-		for (int k = 0; k < i; ++k) {
-			//one byte left shift
-			hold = state[i];
-			for (int j = 0; j < 3; ++j) {
-				state[i + 4 * j] = state[i + 4 * (j + 1)];
-			}
-			state[i + 4 * 3] = hold;
-		}
-	}
-	*/
-	int hold;
-	for (int i = 0; i < 4; ++i) {
+		//i번째 행은(0번부터 시작 시) 총 i번의 right shift가 진행됨을 이용한다.
 		for (int k = 0; k < i; ++k) {
 			//one byte right shift
 			hold = state[i + 4 * 3];
@@ -194,20 +157,8 @@ void shiftRow(unsigned char state[]) {
 	}
 }
 
+//invShiftRow -> ShiftRow의 역연산 -> 왼쪽으로 시프트 연산을 진행해준다.
 void invShiftRow(unsigned char state[]) {
-	/* //debug
-	int hold;
-	for (int i = 0; i < 4; ++i) {
-		for (int k = 0; k < i; ++k) {
-			//one byte right shift
-			hold = state[i + 4 * 3];
-			for (int j = 3; j >= 1; --j) {
-				state[i + 4 * j] = state[i + 4 * (j - 1)];
-			}
-			state[i] = hold;
-		}
-	}
-	*/
 	int hold;
 	for (int i = 0; i < 4; ++i) {
 		for (int k = 0; k < i; ++k) {
@@ -221,6 +172,8 @@ void invShiftRow(unsigned char state[]) {
 	}
 }
 
+//mixColumn -> 표준에 정의된 배열의 값과의 GF(2^8)상에서의 행렬곱셈을 진행한다.
+//계산 중간과정을 이용하기 위해 result라는 추가적인 배열을 사용하였다.
 void mixColumn(unsigned char state[]) {
 	unsigned char result[BLKSIZE];
 	//행렬곱셈을 GF(2^8)에서 진행하였다.
@@ -235,6 +188,7 @@ void mixColumn(unsigned char state[]) {
 	}
 }
 
+//invMixColumn -> mixColumn의 역연산, mixColumn에서 곱한 행렬의 역행렬을 곱함으로써 얻어낼 수 있다.
 void invMixColumn(unsigned char state[]) {
 	unsigned char result[BLKSIZE];
 	for (int i = 0; i < 4; ++i) {
@@ -248,13 +202,14 @@ void invMixColumn(unsigned char state[]) {
 	}
 }
 
+//printHex -> 주어진 target의 값을 hexadecimal의 표현으로 출력한다.
 void printHex(const unsigned char target[]) {
 	for (int i = 0; i < BLKSIZE; ++i)
 		cout << hex << setw(2) << setfill('0') << int(target[i]);
 	cout << endl;
 }
 
-//for debug
+//printBinary -> 주어진 x의 값을 binary 표현으로 출력한다.
 void printBinary(unsigned char x) {
 	string result = "";
 	for (int i = 0; i < 8; ++i) {
@@ -268,14 +223,13 @@ void printBinary(unsigned char x) {
 	cout << result;
 }
 
-
-//return RC[round]
+//getRoundCont -> round에 해당하는 RoundConstant의 값을 반환한다. (재귀적인 방식으로 구현)
 unsigned char getRoundCont(int round) {
 	if (round == 1) return 1;
 	else return multInGf(2, getRoundCont(round - 1));			//2 * RC[i-1]
 }
 
-// *2 over GF(2^8)
+//addRoundCont -> WORD에 주어진 round의 constant값을 더해준다. (이 떄의 덧셈은 GF(2^8) 상의 덧셈이다.)
 WORD addRoundCont(WORD wd, int round) {
 	WORD ret;
 	unsigned char roundCont = getRoundCont(round);
@@ -288,7 +242,7 @@ WORD addRoundCont(WORD wd, int round) {
 	return ret;
 }
 
-//1 byte left shift
+//rotWord -> wd WORD의 모든 바이트를 값을 왼쪽으로 시프트 연산을 해준다.
 WORD rotWord(WORD wd) {
 	WORD ret;
 	unsigned char hold = wd.bytes[0];
@@ -301,15 +255,16 @@ WORD rotWord(WORD wd) {
 	return ret;
 }
 
+//subWord -> 주어진 wd WORD의 값을 Sbox를 이용하여 새롭게 mapping 시켜 나온 WORD의 값으로 변환하여 반환해준다.
 WORD subWord(WORD wd) {
 	WORD ret;
 	for (int i = 0; i < WORDSIZE; ++i) {
 		ret.bytes[i] = SBox[(wd.bytes[i] & 0xF0) + (wd.bytes[i] & 0x0F)];
-		//ret.bytes[i] = r_sbox[(wd.bytes[i] & 0xf0) + (wd.bytes[i] & 0x0f)];			//for debug
 	}
 	return ret;
 }
 
+//keyExpansion -> roundKey를 주어진 key값을 이용하여 wd의 배열에 담아준다.
 void keyExpansion(const unsigned char key[BLKSIZE], WORD wd[WORDSIZE * NUMOFROUNDS]) {
 	WORD temp;
 	for (int i = 0; i < WORDSIZE; ++i) {
@@ -330,102 +285,104 @@ int main() {
 	ofstream cipherOut(cipherTextFile, ios::out | ios::binary);
 	ofstream decryptOut(decrpytFile, ios::out | ios::binary);
 	
-	if (!plaintextIn.is_open() || !keyIn.is_open()) {
+	if (!plaintextIn.is_open() || !keyIn.is_open() || !cipherOut.is_open() || !decryptOut.is_open()) {
 		cerr << "error occur while opening file\n";
 		exit(1);
 	}
 
-	unsigned char key[BLKSIZE];
-	unsigned char state[BLKSIZE];
-	WORD roundKey[WORDSIZE * NUMOFROUNDS];
+	unsigned char key[BLKSIZE];							//입력 키값 저장 배열
+	unsigned char state[BLKSIZE];						//16byte block 저장 배열
+	WORD roundKey[WORDSIZE * NUMOFROUNDS];				//roundKey를 저장할 WORD단위의 배열 (i번 라운드의 key값은 roundKey[4*i ~ 4*i +3]에 위치하게 된다.
 
 	//key 입력 시작
 	for (int i = 0; i < BLKSIZE; ++i)
 		keyIn.read((char*)&key[i], 1);
 
-	keyIn.close();
 	//key 입력 종료
+	keyIn.close();										
 
+	//keyExpansion 수행
 	keyExpansion(key, roundKey);
 
-	//plainText 입력 및 encryption 시작
-	for (int i = 0; i < BLKSIZE; ++i)
-		plaintextIn.read((char*)&state[i], 1);
+	while (true) {
+		//plainText입력
+		for (int i = 0; i < BLKSIZE; ++i)
+			plaintextIn.read((char*)&state[i], 1);
 
-	cout << "Input plain text : ";
-	printHex(state);
-	cout << endl;
+		if (plaintextIn.eof()) break;			//EOF check
 
+		cout << "key: " << key << endl;
+		//plainText 입력 및 encryption 시작
 
-	for (int i = 0; i < NUMOFROUNDS; ++i) {
-		cout << "ROUND " << dec << i << endl;
+		cout << "Input plain text : ";
+		printHex(state);
+		cout << endl;
 
-		if (i != 0) {
-			//SubBytes
-			subByte(state);
-			cout << "\tSB : ";
-			printHex(state);
+		for (int i = 0; i < NUMOFROUNDS; ++i) {
+			cout << "ROUND " << dec << i << endl;
 
-			//ShiftRow
-			shiftRow(state);
-			cout << "\tSR : ";
-			printHex(state);
-
-			if (i != NUMOFROUNDS - 1) {
-				//MixColumn
-				mixColumn(state);
-				cout << "\tMC : ";
+			//0번째 라운드 제외 모든 라운드가 수행하는 부분
+			if (i != 0) {
+				//SubBytes
+				subByte(state);
+				cout << "\tSB : ";
 				printHex(state);
+
+				//ShiftRow
+				shiftRow(state);
+				cout << "\tSR : ";
+				printHex(state);
+
+				//0번, 10번 라운드 제외 모든 라운드가 수행하는 부분
+				if (i != NUMOFROUNDS - 1) {
+					//MixColumn
+					mixColumn(state);
+					cout << "\tMC : ";
+					printHex(state);
+				}
 			}
+
+			//AddRoundKey
+			addRoundKey(roundKey + 4 * i, state);
+			cout << "\tAR : ";
+			printHex(state);
+
 		}
 
-		//AddRoundKey
-		addRoundKey(roundKey + 4 * i, state);
-		cout << "\tAR : ";
+		//ct2.bin 파일에 encrypt된 정보를 저장
+		cipherOut.write((char*)state, BLKSIZE);
+		cout << endl << endl;
+
+		cout << "Result Encryption: ";
 		printHex(state);
 
+		//decryption 시작
+		for (int i = 0; i < NUMOFROUNDS; ++i) {
+			if (i != 0) {
+				//invShiftRow
+				invShiftRow(state);
+
+				//invSubBytes
+				invSubByte(state);
+			}
+
+			//invAddRoundKey  복호화 시에는 키의 순서가 반대임에 주의
+			invAddRoundKey(roundKey + 4 * (NUMOFROUNDS - 1 - i), state);
+
+			if (i != 0 && i != NUMOFROUNDS - 1) {
+				//invMixColumn
+				invMixColumn(state);
+			}
+
+		}
+
+		cout << "Result Decryption: ";
+		printHex(state);
+		decryptOut.write((char*)state, BLKSIZE);			//decryption 결과 출력
 	}
-	//ct2.bin 파일에 값 저장
-	cipherOut.write((char*)state, BLKSIZE);
+
 	cipherOut.close();
-	cout << endl << endl;
-
-	cout << "Result Encryption: ";
-	printHex(state);
-
 	plaintextIn.close();
-	//plainText 입력 및 encryption 종료
-
-	//cipherText 입력 및 decryption 시작
-	ifstream cipherTextIn(cipherTextFile, ios::in | ios::binary);
-
-	for (int i = 0; i < BLKSIZE; ++i)
-		cipherTextIn.read((char*)&state[i], 1);
-
-	
-	for (int i = 0; i < NUMOFROUNDS; ++i) {
-		if (i != 0) {
-			//invShiftRow
-			invShiftRow(state);
-
-			//invSubBytes
-			invSubByte(state);
-		}
-
-		//invAddRoundKey  복호화 시에는 키의 순서가 반대임에 주의
-		invAddRoundKey(roundKey + 4 * (NUMOFROUNDS - 1 - i), state);
-
-		if (i != 0 && i != NUMOFROUNDS - 1) {
-			//invMixColumn
-			invMixColumn(state);
-		}
-		
-	}
-
-	cout << "Result Decryption: ";
-	printHex(state);
-	decryptOut.write((char*)state, BLKSIZE);
-
 	decryptOut.close();
 	return 0;
 }
